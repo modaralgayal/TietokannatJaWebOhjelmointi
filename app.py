ebfc8092d0ccc5ccc2dd9256fc8e29d7e3f68340
modar_algayal <mudar.algayal@helsinki.fi>
2025-06-09 12:20:35 +0300

    created a simple app. Skeleten for the main task

--- app.py
+++ app.py
@@ -1,19 +1,47 @@
 import sqlite3
 
-from flask import Flask, redirect, render_template, request
-from werkzeug.security import generate_password_hash
+from flask import Flask, redirect, render_template, request, session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+import config
+import db
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 
 @app.route("/")
 def index():
-    db = sqlite3.connect("database.db")
-    messages = db.execute("SELECT content FROM messages").fetchall()
+    return render_template("index.html")
+
+
+@app.route("/homepage")
+def homepage():
+    messages = db.query("SELECT content FROM messages")
     count = len(messages)
-    db.close()
     message = "Page has " + str(count) + " messages"
-    return render_template("index.html", message=message, messages=messages)
+    return render_template("homepage.html", message=message, messages=messages)
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    sql_command = "SELECT password_hash FROM users WHERE username = ?"
+    password_hash = db.query(sql_command, [username])[0][0]
+
+    if check_password_hash(password_hash, password):
+        session["username"] = username
+        return redirect("/homepage")
+    else:
+        return "ERROR: Invalid username or password"
+
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
 
 
 @app.route("/register")
@@ -23,7 +51,6 @@ def register():
 
 @app.route("/create", methods=["POST"])
 def create():
-    db = sqlite3.connect("database.db")
     username = request.form["username"]
     password1 = request.form["password1"]
     password2 = request.form["password2"]
@@ -49,10 +76,7 @@ def new():
 def send():
     content = request.form["content"]
     if len(content) > 0:
-        db = sqlite3.connect("database.db")
         db.execute("INSERT INTO messages (content) VALUES (?)", [content])
-        db.commit()
-        db.close()
         return redirect("/")
     return redirect("/")
 
