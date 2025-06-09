7ab0287781fa41b2808159607e2a1e6fa304ce40
modar_algayal <mudar.algayal@helsinki.fi>
2025-06-09 10:19:16 +0300

    user creation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 from flask import Flask, redirect, render_template, request
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
@@ -13,6 +14,30 @@ def index():
     db.close()
     message = "Page has " + str(count) + " messages"
     return render_template("index.html", message=message, messages=messages)
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/create", methods=["POST"])
+def create():
+    db = sqlite3.connect("database.db")
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql_command = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql_command, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "ERROR: user already exists"
+
+    return "User created"
 
 
 @app.route("/new")
